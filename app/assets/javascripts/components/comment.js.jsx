aa583874e4c07c78ac269922dbc07d8737602073
Jon Lebensold <jon@lebensold.ca>
2015-05-29 23:29:31 +0200

    stubbing a simple react component

---
+++ app/assets/javascripts/components/comment.js.jsx
@@ -0,0 +1,17 @@
+var Comment = React.createClass({
+  propTypes: {
+    author: React.PropTypes.string,
+    body: React.PropTypes.string,
+    rank: React.PropTypes.node
+  },
+
+  render: function() {
+    return (
+      <div>
+        <div>Author: {this.props.author}</div>
+        <div>Body: {this.props.body}</div>
+        <div>Rank: {this.props.rank}</div>
+      </div>
+    );
+  }
+});
