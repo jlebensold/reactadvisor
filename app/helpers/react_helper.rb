66ad3439d5aab51f91be9a8cfc79bacfa3f3307c
Jon Lebensold <jon@lebensold.ca>
2015-07-08 14:17:12 +0200

    adding flag for rendering client/server

--- app/helpers/react_helper.rb
+++ app/helpers/react_helper.rb
@@ -1,11 +1,23 @@
 module ReactHelper
+  def client(component, id, props)
+    sc = script(component, id, props)
+    "<div id='#{id}'></div>#{sc}".html_safe
+  end
+
   def isomorphic(component, id, props)
-    script = "<script>
+    sc = script(component, id, props)
+    component = HTTParty.post "http://localhost:3001", body: {component: component, props: props}.to_json, headers: {'Content-Type' => "application/json"}
+    "<div id='#{id}'>#{component}</div>#{sc}".html_safe
+  end
+
+  private
+
+  def script(component, id, props)
+    "<script>
   document.addEventListener(\"DOMContentLoaded\", function(event) {
     renderReact('#{id}', '#{component}', #{props.to_json});
   });
 </script>"
-    component = HTTParty.post "http://localhost:3001", body: {component: component, props: props}.to_json, headers: {'Content-Type' => "application/json"}
-    "<div id='#{id}'>#{component}</div>#{script}".html_safe
   end
+
 end
